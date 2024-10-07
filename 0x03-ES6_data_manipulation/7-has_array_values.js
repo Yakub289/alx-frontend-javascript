673b9bdec25a487bc14d1b2ec26955eaaf351913
Yakub289 <Yakubsuleiman24@gmail.com>
2024-10-07 16:08:06 -0700

    Manipulating

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 export default function hasValuesFromArray(set, array) {
-  const containValue = array.every((value) => set.has(value));
-  return conainValue;
+  const hasElements = array.every((value) => set.has(value));
+  return hasElements;
 }
